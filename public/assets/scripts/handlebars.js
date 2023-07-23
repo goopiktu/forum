b516106190a48cc4705228fd0be6b4adbb1d298e
jacyong <90311432+kurochiii@users.noreply.github.com>
2023-07-23 05:35:34 -0700

    added comment handlebar

--- public/assets/scripts/handlebars.js
+++ public/assets/scripts/handlebars.js
@@ -1,18 +1,17 @@
 
-
 //edit post 
-let postTemp = document.querySelector("#contentTemp").innerHTML; 
+let postTemp = document.querySelector("#postTemp").innerHTML; 
 
 let posttempFunc = Handlebars.compile(postTemp);
 
 let postCont = {
+    username: "nootdoot", 
     title: "Goblin",
     datePosted: "June 7, 2023",
-    username: "nootdoot", 
-    upvote: "3", 
-    downvote: "35",
     body:"This part here can ow HTML tags",
     edited:"(edited)", 
+    upvote: 3, 
+    downvote: 35
   };
 
 let tempPostCont = posttempFunc(postCont);
@@ -20,22 +19,28 @@ let tempPostCont = posttempFunc(postCont);
 document.querySelector("#postContent").innerHTML = tempPostCont;
 
 
+const Comment = function(commentID)
+{
+    
+
+}
+
 // edit comment
 
-// let postTemp = document.querySelector("#contentTemp").innerHTML; 
+let commentTemp = document.querySelector("#commentTemp").innerHTML; 
 
-// let posttempFunc = Handlebars.compile(postTemp);
+let commenttempFunc = Handlebars.compile(commentTemp);
 
-// let temp1obj1 = {
-//     title: "Goblin",
-//     datePosted: "June 7, 2023",
-//     username: "nootdoot", 
-//     upvote: "3", 
-//     downvote: "35",
-//     body:"This part here can ow HTML tags", 
+let commentCont = {
+    username: "pootTroot", 
+    datePosted: "Dat", 
+    body: "my PrEciOussss", 
+    edited: Number, 
+    upvote: 0, 
+    downvote: 0
+  };
 
-//   };
+let tempCommentCont = commenttempFunc(commentCont);
 
-// let temp1FromObj1 = posttempFunc(temp1obj1);
-
-// document.querySelector("#postContent").innerHTML = temp1FromObj1;
+document.querySelector("#commentBox").innerHTML += tempCommentCont;
+document.querySelector("#commentBox").innerHTML += tempCommentCont;
