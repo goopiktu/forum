f25aa42518f636d93d2c3de06ec781d0ccb2deaa
jacyong <90311432+kurochiii@users.noreply.github.com>
2023-07-24 09:13:04 -0700

    added first part of router

--- routes/route.js
+++ routes/route.js
@@ -5,3 +5,12 @@ var schemas = require('../models/schemas');
 
 schemas.userposts.collection.insertOne();
 
+const express = require(`express`);
+const signinController = require(`../controllers/signinController`);
+
+const app = express();
+
+app.get('/', signinController.getSignIn);
+app.post('/', signinController.postLogIn);
+
+module.exports = app;
