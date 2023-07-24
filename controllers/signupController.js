5e3cc156fa0011c42acacdd7ca1d2885be2abbd6
nicoleUY <nicole_uy@dlsu.edu.ph>
2023-07-25 01:48:52 +0800

    updated signupcontroller
    added conditions, pero tb idk rin if tama

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -4,26 +4,54 @@ const User = require('../models/UserModel.js');
 const signupController = {
 
     getSignUp: function (req, res) {
-        res.render('SignUp',res);
+        res.render("register", {layout: 'signInReg'});
     },
 
     postSignUp: async function (req, res) {
+        const email = req.body.email;
+        const name = req.body.username;
+        const pass = req.body.password;
+        const confirm = req.body.confirm;
+
         const user = {
-            email: req.body.email,
-            username: req.body.username,
-            password: req.body.password,
-            confirm: req.body.confirm,
+            email: email,
+            username: name,
+            password: pass,
+            confirm: confirm,
         }
+
+        try {
+            const query = {username: name, password: pass};
+            const projection = {username:1};
+            const result = await db.findOne(User,query,projection);
+
+            if (result){
+                // username already exists
+                alert("Username already exists");
+                // res.redirect('/register');
+            } else if (pass.length < 8){
+                alert("Your password needs at least 8 characters");
+            } else if (pass !== confirm) {
+                alert("The passwords you have inputted do not match");
+            } else {
+                var success = await db.insertOne(User, user);
+                if( success ){
+                    console.log('User successfully added');
+                    res.render('signIn', {isRegistered: true});
+                }
+                else{
+                    console.log('User not added');
+                }
+                // res.render('signIn', {isValid: true});
+            }
+        } catch (err){
+            res.status(500).send(err);
+        }
+
 
         var success = await db.insertOne(User, user);
 
-        if( success ){
-            console.log('User successfully added');
-            res.render('signIn', {isRegistered: true});
-        }
-        else{
-            console.log('User not added');
-        }
+        
     }
 
 }
