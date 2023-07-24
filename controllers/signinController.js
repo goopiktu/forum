d2e6c49bda6ab5dedf7222db300b637d05f65be0
nicoleUY <nicole_uy@dlsu.edu.ph>
2023-07-25 00:40:31 +0800

    signin contoller updated

--- controllers/signinController.js
+++ controllers/signinController.js
@@ -1,12 +1,31 @@
+// import scemas
 const db = require('../models/db.js');
-const 
-
+const User = require('../models/UserModel.js');
+ 
 const signinController = {
     getSignIn : function (req, res){
         res.render("signIn", {layout: 'signinReg'});
-    }
+    },
    
+    // dis part i am not rlly sure of ehe
+    postSignIn: async function (req, res){
+        const name = req.body.username;
+        const pass = req.body.password;
 
-}
+        try{
+            const query = {username: name, password: pass};
+            const projection = {username:1};
+            const result = await db.findOne(User,query,projection);
 
-module.exports = controller;
+            if (result){
+                res.redirect('/logged_in');
+            } else {
+                res.render('signIn', {isValid: false});
+            }
+        } catch (err){
+            res.status(500).send(err);
+        }
+    }
+};
+
+module.exports = signinController;
