368a7309cddc2e20af0c44a3b3b49b03926ca59c
jacyong <90311432+kurochiii@users.noreply.github.com>
2023-07-24 22:18:17 -0700

    testes in hbs was flexible

--- controllers/homepageController.js
+++ controllers/homepageController.js
@@ -1,17 +1,26 @@
 const db = require('../models/db.js');
-const post = require('../models/PostModel.js');
+// const post = require('../models/PostModel.js');
  
 const homepageController = {
     guestView : function (req, res){
         var info = {
             posts: [
                 {
+                    title: "helow bost", 
                     username: "Current User", 
                     datePosted: new Date(),   
                     upvote: 2, 
                     downvote: 3
                 }, 
                 {
+                    title: "U R GEI",
+                    username: "Nerzik", 
+                    datePosted: "July 12, 2003", 
+                    upvote: 78, 
+                    downvote: 3
+                }, 
+                {
+                    title: "PeePEe POo Poo",
                     username: "Nerzik", 
                     datePosted: "July 12, 2003", 
                     upvote: 78, 
