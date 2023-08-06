4f1f31cb7b3646cc94cf2f741850864783c0030e
jacyong <90311432+kurochiii@users.noreply.github.com>
2023-08-06 15:46:00 +0800

    edited stuffs

--- FINALindex.js
+++ FINALindex.js
@@ -48,7 +48,7 @@ app.engine("hbs", exphbs.engine({
             if (arg === commenter){
              return "<a href='" + linkIndex + "/" + index + "/editComment'><button class='rep-button'>Edit</button></a><button class='rep-button delete-button' data-commentid='{{this._id}}' method='DELETE'>Delete</button>"
             }
-        }, 
+        }
     }
 }));
 
